e4ed39b3dd4245199601d3a9faae910ff3003134
Muhammed Faraan <faraanshaik19@gmail.com>
2024-02-02 02:08:53 +0530

    Add files via upload

--- src/screens/Products.js
+++ src/screens/Products.js
@@ -2,17 +2,22 @@ import React, { useEffect, useState } from "react";
 import DisplayProducts from "./DisplayProducts";
 import ProductsApi from "../api/ProductsApi";
 import Footer from "./Footer";
-//import axios from "axios";
+import Spinner from "./Spinner";
 
 export default function Products({ cartApi, setCartApi }) {
   const [products, setProducts] = useState([]);
 
+  const [loading, setLoading] = useState(false);
+
   const getProducts = async () => {
     const response = await ProductsApi();
     setProducts(response);
+    setLoading(false);
   };
+
   useEffect(() => {
     getProducts();
+    setLoading(true);
   }, []);
 
   // const getProducts = async () => {
@@ -28,20 +33,24 @@ export default function Products({ cartApi, setCartApi }) {
 
   return (
     <>
-      <div className="container">
-        <div className="row">
-          {products.length > 0 &&
-            products.map((product) => (
-              <div className="col-sm-6 col-md-4 col-lg-3" key={product.id}>
-                <DisplayProducts
-                  item={product}
-                  cartApi={cartApi}
-                  setCartApi={setCartApi}
-                />
-              </div>
-            ))}
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="container">
+          <div className="row">
+            {products.length > 0 &&
+              products.map((product) => (
+                <div className="col-sm-6 col-md-4 col-lg-3" key={product.id}>
+                  <DisplayProducts
+                    item={product}
+                    cartApi={cartApi}
+                    setCartApi={setCartApi}
+                  />
+                </div>
+              ))}
+          </div>
         </div>
-      </div>
+      )}
       <Footer />
     </>
   );
