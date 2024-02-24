71176d54b4ef3db4eb46583b7c672d5f5d2ebf07
Muhammed Faraan <faraanshaik19@gmail.com>
2024-02-24 20:15:36 +0530

    Add files via upload
    commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+		<ToastContainer
+			position="top-right"
+			autoClose={2000}
+			hideProgressBar={false}
+			newestonTop={false}
+			closeOnClick
+			rtl={false}
+			pauseOnFocusLoss
+			draggable
+			pauseOnHover
+			theme="colored"
+		/>
+	</React.StrictMode>
 );
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -3,74 +3,70 @@ import "../styles/login.css";
 import loginApi from "../api/LoginApi";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export default function Login() {
-  const [data, setData] = useState({
-    email: "",
-    password: "",
-  });
+	const [data, setData] = useState({
+		email: "",
+		password: "",
+	});
 
-  const { email, password } = data;
-  //console.log(data);
+	const { email, password } = data;
+	//console.log(data);
 
-  const handleChange = (e) => {
-    setData({ ...data, [e.target.name]: e.target.value });
-  };
+	const handleChange = (e) => {
+		setData({ ...data, [e.target.name]: e.target.value });
+	};
 
-  const navigate = useNavigate();
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+	const navigate = useNavigate();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-    const response = await loginApi(data);
-    if (!response.token) {
-      alert(response.data.message);
-      return false;
-    } else navigate("/");
-    // setting token in local storage to access it later
-    localStorage.setItem("token", response.token);
-    // console.log(response);
-  };
-  return (
-    <>
-      <hr />
-      <body className="classbody">
-        <div className="firstHalf"></div>
-        <div className="wrapper">
-          <h3 className="hclass">Login</h3>
-          <form onSubmit={handleSubmit}>
-            <div className="input-box">
-              <input
-                type="text"
-                placeholder="Enter your email"
-                name="email"
-                value={email}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="input-box">
-              <input
-                type="password"
-                placeholder="Enter password"
-                name="password"
-                value={password}
-                onChange={handleChange}
-              />
-            </div>
-            {/* <div className="policy">
+		const response = await loginApi(data);
+		if (!response.token) {
+			toast.error(response.data.message);
+			return false;
+		} else navigate("/");
+		// setting token in local storage to access it later
+		localStorage.setItem("token", response.token);
+		// console.log(response);
+		toast.success("login Successfull");
+	};
+	return (
+		<>
+			<hr />
+			<body className="classbody">
+				<div className="firstHalf"></div>
+				<div className="wrapper">
+					<h3 className="hclass">Login</h3>
+					<form onSubmit={handleSubmit}>
+						<div className="input-box">
+							<input type="text" placeholder="Enter your email" name="email" value={email} onChange={handleChange} />
+						</div>
+						<div className="input-box">
+							<input
+								type="password"
+								placeholder="Enter password"
+								name="password"
+								value={password}
+								onChange={handleChange}
+							/>
+						</div>
+						{/* <div className="policy">
               <input type="checkbox" />
               <h3>I accept all terms & condition</h3>
             </div> */}
-            <div className="input-box button">
-              <input type="Submit" value="Login" />
-            </div>
-            <div className="text">
-              <h3>
-                Need an account? <Link to="/registration">Register now</Link>
-              </h3>
-            </div>
-          </form>
-        </div>
-      </body>
-    </>
-  );
+						<div className="input-box button">
+							<input type="Submit" value="Login" />
+						</div>
+						<div className="text">
+							<h3>
+								Need an account? <Link to="/registration">Register now</Link>
+							</h3>
+						</div>
+					</form>
+				</div>
+			</body>
+		</>
+	);
 }
